f05b7e40b107e8124d2919bf66e25ad5562af782
Ramil Safin <rs0x069@gmail.com>
2022-07-27 00:21:01 +0300

    Disable connection STATIC_URL and STATIC_ROOT

--- python_meetup/urls.py
+++ python_meetup/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
