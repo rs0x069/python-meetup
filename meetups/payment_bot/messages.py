e58d2e6459a76ec8429e40aad69b7697c37a0eaa
alexanderkaptsov <kaptsovaa@gmail.com>
2022-07-29 16:33:16 +0400

    payment bot initial commit

---
+++ meetups/payment_bot/messages.py
@@ -0,0 +1,26 @@
+help_message = '''
+Отправьте команду /donate.
+Правила - по кнопке /terms
+'''
+
+start_message = f'Добро пожаловать в донаты {help_message}'
+
+pre_donate_demo_alert = 'Оплатите донат в сумме:'
+
+terms = 'Правила пользования'
+
+donate_title = 'Донат организатору'
+donate_description = 'Спасибо что поддерживаете нас денешкой!'
+
+successful_payment = 'Платеж на сумму {total_amount} {currency} совершен успешно!'
+
+
+MESSAGES = {
+    'start': start_message,
+    'help': help_message,
+    'pre_donate_demo_alert': pre_donate_demo_alert,
+    'terms': terms,
+    'donate_title': donate_title,
+    'donate_description': donate_description,
+    'successful_payment': successful_payment,
+}
