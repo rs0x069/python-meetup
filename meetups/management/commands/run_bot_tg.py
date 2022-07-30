c65abc4948537d887b8960af427d07823497f2e4
Ramil Safin <rs0x069@gmail.com>
2022-07-30 11:50:56 +0300

    Delete comments

--- meetups/management/commands/run_bot_tg.py
+++ meetups/management/commands/run_bot_tg.py
@@ -1,8 +1,6 @@
 from django.core.management.base import BaseCommand
 from environs import Env
-from telegram import Bot
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from telegram.utils.request import Request
 
 
 def start_command(update, context):
@@ -21,11 +19,6 @@ class Command(BaseCommand):
         env.read_env()
 
         telegram_token = env("TELEGRAM_TOKEN")
-
-        # request = Request(connect_timeout=0.5, read_timeout=1.0)
-        # bot = Bot(request=request, token=telegram_token)
-        #
-        # print(bot.getMe())
 
         updater = Updater(telegram_token)
         dispatcher = updater.dispatcher
