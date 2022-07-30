959eac3d9c63befbcf6aaae2646930ec348c82af
Ramil Safin <rs0x069@gmail.com>
2022-07-30 14:03:21 +0300

    Start creating backend

--- meetups/views.py
+++ meetups/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from meetups.models import Event, Section, Visitor, Question
 
-# Create your views here.
+
+def get_active_events():
+    return Event.objects.filter(is_closed=False)
+
+
+def get_event_info_with_section(event_id):
+    event = Event.objects.get(pk=event_id)
+    return event
+
+
+# from meetups.views import *
